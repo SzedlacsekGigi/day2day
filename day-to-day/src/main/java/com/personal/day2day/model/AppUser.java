a28eeac44040457b377c0717669d394767f3f3ab
SzedlacsekGigi <szedlacsek.lilla@gmail.com>
2020-04-10 16:46:25 +0200

    AppUser model

--- day-to-day/src/main/java/com/personal/day2day/model/AppUser.java
+++ day-to-day/src/main/java/com/personal/day2day/model/AppUser.java
@@ -1,4 +1,28 @@
 package com.personal.day2day.model;
 
+import lombok.*;
+
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.List;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class AppUser {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String username;
+    private String password;
+
+    @ElementCollection
+    @Singular
+    private List<String> roles;
 }
