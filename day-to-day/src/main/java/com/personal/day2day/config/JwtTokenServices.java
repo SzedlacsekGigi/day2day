89b3488abad1de9281db4be92fb376a5ed2427f8
SzedlacsekGigi <szedlacsek.lilla@gmail.com>
2020-04-10 17:33:49 +0200

    Added JwtTokenServices and ValidationController

---
+++ day-to-day/src/main/java/com/personal/day2day/config/JwtTokenServices.java
@@ -0,0 +1,91 @@
+package com.personal.day2day.config;
+
+import io.jsonwebtoken.*;
+import lombok.Data;
+import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Base64;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+@Component
+@Slf4j
+public class JwtTokenServices {
+
+    @Value("${security.jwt.token.secret-key:secret}")
+    private String secretkey = "secret";
+
+    @Value("${security.jwt.token.expire-length:3600000}")
+    private long validityInMilliseconds = 3600000; // 10hours
+
+    private final String rolesFieldName = "roles";
+
+    @PostConstruct
+    protected void init() {
+        secretkey = Base64.getEncoder().encodeToString(secretkey.getBytes());
+    }
+
+    //Creates JWT Token
+    public String createToken(String username, List<String> roles){
+        //Add custom field to the token
+        Claims claims = Jwts.claims().setSubject(username);
+        claims.put(rolesFieldName, roles);
+
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + validityInMilliseconds);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(SignatureAlgorithm.HS256, secretkey)
+                .compact();
+    }
+
+    String getTokenFromRequest(HttpServletRequest req){
+        String bearerToken = req.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Berer ")){
+            return bearerToken.substring(7, bearerToken.length());
+        }
+        return null;
+    }
+
+
+    //Check the token for validity and expiration
+    boolean validateToken(String token){
+        try {
+            Jws<Claims> claims = Jwts.parser().setSigningKey(secretkey).parseClaimsJws(token);
+            return !claims.getBody().getExpiration().before(new Date());
+        } catch (JwtException | IllegalArgumentException e){
+            log.debug("JWT token invalid " + e);
+        }
+        return false;
+    }
+
+    /**
+     * Parses the username and roles from the token. Since the token is signed we can be sure its valid information.
+     * Note that it does not make a DB call to be super fast!
+     * This could result in returning false data (e.g. the user was deleted, but their token has not expired yet)
+     * To prevent errors because of this make sure to check the user in the database for more important calls!
+     */
+
+    Authentication parseUserFromTokenInfo(String token) throws UsernameNotFoundException{
+        Claims body = Jwts.parser().setSigningKey(secretkey).parseClaimsJws(token).getBody();
+        String username = body.getSubject();
+        List<String> roles = (List<String>) body.get(rolesFieldName);
+        List<SimpleGrantedAuthority> authorities = new LinkedList<>();
+        for (String role:roles) {
+            authorities.add(new SimpleGrantedAuthority(role));
+        }
+        return new UsernamePasswordAuthenticationToken(username, "", authorities);
+    }
+}
---
+++ day-to-day/src/main/java/com/personal/day2day/controller/ValidationController.java
@@ -0,0 +1,21 @@
+package com.personal.day2day.controller;
+
+import com.personal.day2day.config.JwtTokenServices;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping("/validation")
+public class ValidationController {
+
+    private final AuthenticationManager authenticationManager;
+
+    private final JwtTokenServices jwtTokenServices;
+
+    public ValidationController(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+}
