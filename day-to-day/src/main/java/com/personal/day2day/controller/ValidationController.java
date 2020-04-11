65ab4ef0556f626cdd1c6e830d435f3f140bc695
SzedlacsekGigi <szedlacsek.lilla@gmail.com>
2020-04-11 08:46:46 +0200

    Registration and login endpoints added to validation controller

--- day-to-day/src/main/java/com/personal/day2day/controller/ValidationController.java
+++ day-to-day/src/main/java/com/personal/day2day/controller/ValidationController.java
@@ -1,10 +1,24 @@
 package com.personal.day2day.controller;
 
 import com.personal.day2day.config.JwtTokenServices;
+import com.personal.day2day.model.UserCredentials;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @Slf4j
@@ -18,5 +32,35 @@ public class ValidationController {
     public ValidationController(AuthenticationManager authenticationManager, JwtTokenServices jwtTokenServices) {
         this.authenticationManager = authenticationManager;
         this.jwtTokenServices = jwtTokenServices;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody UserCredentials data) {
+        try {
+            String username = data.getUsername();
+            Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, data.getPassword()));
+            List<String> roles = authentication.getAuthorities()
+                    .stream()
+                    .map(GrantedAuthority::getAuthority)
+                    .collect(Collectors.toList());
+
+            String token = jwtTokenServices.createToken(username, roles);
+
+            Map<Object, Object> model = new HashMap<>();
+            model.put("username", username);
+            model.put("roles", roles);
+            model.put("token", token);
+            return ResponseEntity.ok(model);
+        } catch (AuthenticationException e) {
+            throw new BadCredentialsException("Invalid username/password supplied");
+        }
+    }
+
+    @PostMapping("/registration")
+    public void registration(@RequestBody UserCredentials data) {
+        log.info(data.toString());
+        String username = data.getUsername();
+        String password = data.getPassword();
+        userService.saveNewUser(username, password);
     }
 }
