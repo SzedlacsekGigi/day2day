beaea425eb0cce99fa3c3f1cd607ca4deb9ab8f1
SzedlacsekGigi <szedlacsek.lilla@gmail.com>
2020-04-11 08:25:42 +0200

    Searching by username added to the user repo

--- day-to-day/src/main/java/com/personal/day2day/repositories/AppUserRepository.java
+++ day-to-day/src/main/java/com/personal/day2day/repositories/AppUserRepository.java
@@ -3,5 +3,10 @@ package com.personal.day2day.repositories;
 import com.personal.day2day.model.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AppUserRepository extends JpaRepository<AppUser, Integer> {
+
+    Optional<AppUser> findByUsername(String username);
+
 }
