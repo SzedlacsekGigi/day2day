3d4d572d21b3eaf565e3a7cf246c0caf115f0d8a
SzedlacsekGigi <szedlacsek.lilla@gmail.com>
2020-04-14 07:28:06 +0200

    Header added with login, logout and registration links added with the condition what to return based on localstorage

--- day-to-day-front/src/App.js
+++ day-to-day-front/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
-import {Content, Header, Navigation} from "react-mdl";
+import {Content, Header} from "react-mdl";
 import Main from "./components/Main";
 import {Layout} from "react-mdl";
 import {Link} from "react-router-dom";
+import AppHeader from "./components/AppHeader";
 
 
 function App() {
@@ -11,14 +12,7 @@ function App() {
         <div className="App">
             <Layout>
                 <Header className="header" title={<Link style={{textDecoration: 'none', color: 'white', fontFamily: 'Advent Pro', fontSize: '40px', fontWeight: 'bold'}} to="/">Brave New World</Link>} scroll>
-                    <Navigation className="nav-header">
-                        <Link className="nav-link" to="/activities">My Activities</Link>
-                        <Link className="nav-link" to="/today">Today</Link>
-                        <Link className="nav-link" to="/calendar">Calendar</Link>
-                        <Link className="nav-link" to="/statistics">Stats</Link>
-                        <Link className="nav-link" to="registration">Registration</Link>
-                        <Link className="nav-link" to="/login">Login</Link>
-                    </Navigation>
+                    <AppHeader/>
                 </Header>
                 <Content style={{width: '100%', height: '100%'}}>
                     <Main/>
---
+++ day-to-day-front/src/components/AppHeader.js
@@ -0,0 +1,40 @@
+import React, {Component} from "react";
+import {Link} from "react-router-dom";
+import {Navigation} from "react-mdl";
+
+class AppHeader extends Component{
+    logout(e) {
+        e.preventDefault();
+        localStorage.removeItem("usertoken");
+        this.props.history.push("/");
+    };
+
+    render() {
+        const regLink = (
+            <Link className="nav-link" to="registration">Registration</Link>
+        );
+        const loginLink = (
+            <Link className="nav-link" to="/login">Login</Link>
+        );
+        const regLoginLink = (
+            regLink, loginLink
+        );
+        const logoutLink = (
+            <Link className="nav-link" to="/" onClick={this.logout.bind(this)}>Logout</Link>
+        );
+
+        return (
+            <div>
+                <Navigation className="nav-header">
+                    <Link className="nav-link" to="/activities">My Activities</Link>
+                    <Link className="nav-link" to="/today">Today</Link>
+                    <Link className="nav-link" to="/calendar">Calendar</Link>
+                    <Link className="nav-link" to="/statistics">Stats</Link>
+                    {localStorage.usertoken ? regLoginLink : logoutLink}
+                </Navigation>
+            </div>
+        );
+    }
+}
+
+export default AppHeader;
